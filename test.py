d894e9b2ca01588153a99b1902c4a85da8768806
maximk1994 <maximk@uw.edu>
2020-03-10 13:13:01 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+#os.system("lava-test-case %s --result %s" % (name, result))
+#print("%s --result %s" % (name, result))
+
+import os
+
+def main():
+	output = os.popen('uname -a').read()
+
+	if 'Linux' in output:
+#		print('Pass')
+		return True
+	else:
+#		print('Fail')
+		return False
+
+if __name__ == '__main__':
+	result = main()
+
+	if result:
+		print('pass')
+		os.system("lava-test-case pTest --result pass")
+	else:
+		print('fail')
+		os.system("lava-test-case pTest --result fail")
+
